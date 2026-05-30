646a3fdb01b301e95df8998359053e2c90a86272
Vesa <vesa@Vesas-MacBook-Air.local>
2026-05-31 01:33:14 +0200

    add bookmarks persistence and controlBookmarks

--- src/js/model.js
+++ src/js/model.js
@@ -107,3 +107,14 @@ export const deleteBookmark = function (id) {
 
   persistBookmarks();
 };
+
+const init = function () {
+  const storage = localStorage.getItem('bookmarks');
+  if (storage) state.bookmarks = JSON.parse(storage);
+};
+init();
+
+const clearBookmarks = function () {
+  localStorage.clear('bookmarks');
+};
+// clearBookmarks();
