fb4cfaad72225c0f840c78d8a804faecf397da39
Vesa <vesa@Vesas-MacBook-Air.local>
2026-05-31 13:24:07 +0200

    update author name in View.js

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -9,7 +9,7 @@ export default class View {
    * @param {boolean} [render=true] If false, create markup string instead of rendering to the DOM
    * @returns {undefined | string} A markup string is returned if render=false
    * @this {Object} View instance
-   * @author Jonas Schmedtmann
+   * @author Vesa Emini
    * @todo Finish implementation
    */
   render(data, render = true) {
